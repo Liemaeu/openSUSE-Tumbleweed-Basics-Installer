0cd7c11c10a36b2d79509bbf717ae5408d422ccc
Liemaeu <30625460+Liemaeu@users.noreply.github.com>
2021-06-17 10:53:58 +0200

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#Adds packman repo and installs codecs
+sudo zypper --non-interactive --quiet addrepo --refresh -p 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ 'packman'
+sudo zypper --gpg-auto-import-keys refresh
+sudo zypper --non-interactive dist-upgrade --allow-vendor-change --from packman
+sudo zypper --non-interactive install -y --from packman ffmpeg gstreamer-plugins-{good,bad,ugly,libav} libavcodec-full vlc-codecs
+
+#disables boot timeout
+sudo sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/" /etc/default/grub
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+#auto unlocks KWallet
+sudo zypper --non-interactive install -y pam_kwallet
+
+#installs and auto unlocks Gnome-Keyring
+sudo zypper --non-interactive install -y gnome-keyring seahorse
+sed -i "/common-auth/a auth	optional	pam_gnome_keyring.so" /etc/pam.d/sddm
+sed -i "/common-session/a session	optional	pam_gnome_keyring.so auto_start" /etc/pam.d/sddm
+
+#sets YaST Software as default application for .rpm
+sed -i "/Default Applications/a application/x-rpm=org.opensuse.yast.Packager.desktop;" ~/.config/mimeapps.list
