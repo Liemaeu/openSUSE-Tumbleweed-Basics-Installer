18fb3f18622643fd156a47085453f357b28ea92c
Liemaeu <30625460+Liemaeu@users.noreply.github.com>
2022-01-24 17:00:16 +0100

    added cups, firewall zone and acpi errors

--- Root-Commands.sh
+++ Root-Commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #opens KDialog progress window
-dbusRef=`kdialog --title "Installing, please wait" --progressbar "Starting..." 12`
+dbusRef=`kdialog --title "Installing, please wait" --progressbar "Starting..." 15`
 qdbus-qt5 $dbusRef showCancelButton false
 
 qdbus-qt5 $dbusRef Set "" value 1
@@ -27,29 +27,51 @@ qdbus-qt5 $dbusRef setLabelText "Installing Build Tools..."
 zypper --non-interactive install -y patterns-devel-base-devel_basis
 
 qdbus-qt5 $dbusRef Set "" value 5
+qdbus-qt5 $dbusRef setLabelText "Installing CUPS..."
+#installs cups
+zypper --non-interactive install -y cups
+groupadd lpadmin
+usermod -a -G lpadmin $USER
+sed -i "s/^SystemGroup root.*/SystemGroup lpadmin/" /etc/cups/cups-files.conf
+
+qdbus-qt5 $dbusRef Set "" value 6
 qdbus-qt5 $dbusRef setLabelText "Removing Discover..."
 #removes discover
 zypper --non-interactive remove -y discover
 
-qdbus-qt5 $dbusRef Set "" value 6
+qdbus-qt5 $dbusRef Set "" value 7
 qdbus-qt5 $dbusRef setLabelText "Disabeling Boot Delay..."
 #disables boot timeout
 sed -i "s/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/" /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
-qdbus-qt5 $dbusRef Set "" value 7
+qdbus-qt5 $dbusRef Set "" value 8
+qdbus-qt5 $dbusRef setLabelText "Setting up Firewall..."
+#sets firewall zone to trusted
+sed -i "s/^DefaultZone=public.*/DefaultZone=trusted/" /etc/firewalld/firewalld.conf
+systemctl restart firewalld
+
+qdbus-qt5 $dbusRef Set "" value 9
+qdbus-qt5 $dbusRef setLabelText "Disabling Boot Messages..."
+#hides acpi errors
+if ! grep -Fxq "loglevel=3" "/etc/default/grub" ; then
+  sed -i s/quiet/& loglevel=3/’ /etc/default/grub
+  grub2-mkconfig -o /boot/grub2/grub.cfg
+fi
+
+qdbus-qt5 $dbusRef Set "" value 10
 qdbus-qt5 $dbusRef setLabelText "Decreasing Swappiness..."
 #decreases swappiness
 if ! grep -Fxq "vm.swappiness=10" "/etc/sysctl.conf" ; then
   "vm.swappiness=10" >> /etc/sysctl.conf
 fi
 
-qdbus-qt5 $dbusRef Set "" value 8
+qdbus-qt5 $dbusRef Set "" value 11
 qdbus-qt5 $dbusRef setLabelText "Enabling autounlock for KWallet..."
 #auto unlocks KWallet
 zypper --non-interactive install -y pam_kwallet
 
-qdbus-qt5 $dbusRef Set "" value 9
+qdbus-qt5 $dbusRef Set "" value 12
 qdbus-qt5 $dbusRef setLabelText "Installing Gnome Keyring..."
 #installs and auto unlocks Gnome-Keyring
 zypper --non-interactive install -y gnome-keyring seahorse
@@ -58,7 +80,7 @@ if ! grep -Fxq "pam_gnome_keyring.so" "/etc/pam.d/sddm" ; then
   sed -i "/common-session/a session	optional	pam_gnome_keyring.so auto_start" /etc/pam.d/sddm
 fi
 
-qdbus-qt5 $dbusRef Set "" value 10
+qdbus-qt5 $dbusRef Set "" value 13
 qdbus-qt5 $dbusRef setLabelText "Installing Snap..."
 #installs snap
 zypper --non-interactive addrepo --refresh https://download.opensuse.org/repositories/system:/snappy/openSUSE_Tumbleweed snappy
@@ -68,13 +90,13 @@ zypper --non-interactive install snapd
 systemctl enable --now snapd
 systemctl enable --now snapd.apparmor
 
-qdbus-qt5 $dbusRef Set "" value 11
+qdbus-qt5 $dbusRef Set "" value 14
 qdbus-qt5 $dbusRef setLabelText "Installing Flatpak..."
 #installs flatpak
 zypper --non-interactive install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-qdbus-qt5 $dbusRef Set "" value 12
+qdbus-qt5 $dbusRef Set "" value 15
 qdbus-qt5 $dbusRef setLabelText "Updating System..."
 #installs updates
 zypper --non-interactive refresh
