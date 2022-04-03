442cb63325efcf2b1ee61c30d7832ed390e23cb7
Liemaeu <30625460+Liemaeu@users.noreply.github.com>
2022-04-03 13:33:51 +0200

    fix for swappiness

--- Root-Commands.sh
+++ Root-Commands.sh
@@ -62,7 +62,7 @@ fi
 qdbus-qt5 $dbusRef Set "" value 10
 qdbus-qt5 $dbusRef setLabelText "Decreasing Swappiness..."
 #decreases swappiness
-if ! grep -Fxq "vm.swappiness=10" "/etc/sysctl.conf" ; then
+if ! grep -Fxq "vm.swappiness" "/etc/sysctl.conf" ; then
   "vm.swappiness=10" >> /etc/sysctl.conf
 fi
 
